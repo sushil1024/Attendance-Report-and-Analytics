407c5948e96b3da2662dd8fbfffc96fa729b8bde
sushil1024 <sushiltgr07@gmail.com>
2021-10-30 22:56:09 +0530

    Merge remote-tracking branch 'origin/master'

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request, File, UploadFile, Form
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 
+
 app = FastAPI()
 templates = Jinja2Templates(directory="templates/")
 
@@ -11,6 +12,16 @@ async def something(request: Request):
     return templates.TemplateResponse("home.html", {"request": request})
 
 
-@app.post("/inputs")
-async def inputdata(request: Request):
-    return templates.TemplateResponse("input.html")
+@app.post("/login/{username}")
+def authent(username: str, password: str):
+    if username == "root" and password == "root":
+        return "Access Granted"
+
+        @app.post("/inputs")
+        async def inputdata(request: Request, rollno: int):
+            return templates.TemplateResponse("input.html", {"request": Request, "StudentRollNo": rollno})
+
+    else:
+        return "Inavlid Credentials"
+
+
