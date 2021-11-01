1f1b87de65c096653f8b3d377b927845681919dc
sushil1024 <sushiltgr07@gmail.com>
2021-11-01 06:27:10 +0530

    Merge remote-tracking branch 'origin/master'

--- searchdata.py
+++ searchdata.py
@@ -3,17 +3,17 @@ import cgi
 from IPython.display import HTML
 
 
-form = cgi.FieldStorage()
-
 # opening file
 file = pd.read_csv('records.csv')
 temp = None
+
 
 def inputroll(roll):
     # accepting roll number
     # roll = int(input("Enter roll no to search: "))
     if roll is not None:
         try:
+            roll = int(roll)
             with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', None):
                 temp = file.loc[[roll - 1]]  # temp variable stores value of the dataframe
                 # print(file.loc[[roll - 1]])  # add function to process
@@ -30,7 +30,6 @@ def inputroll(roll):
             dob = df.iloc[roll - 1, 6]
             age = df.iloc[roll - 1, 5]
             gender = df.iloc[roll - 1, 1]
-
             country = df.iloc[roll - 1, 3]
             city = df.iloc[roll - 1, 2]
 
@@ -46,20 +45,24 @@ def inputroll(roll):
             age = str(age)
             lecs = str(lecs)
 
-            mailch = input(str("Do you want the report sent on the given email id?(y/n): "))
-            if mailch == 'y' or mailch == 'Y':
-                from genreport import genpdf
-
-                genpdf(studentName, roll, dob, age, gender, residence, lecs)
-
-                from mailto import sendmail
-
-                out1 = sendmail(emailid, studentName)
-                print(f"Report sent to " + "'" + emailid + "'")
-
-            else:
-                print("Report not sent!")
+            # mailch = input(str("Do you want the report sent on the given email id?(y/n): "))
+            # if mailch == 'y' or mailch == 'Y':
+            #     from genreport import genpdf
+            #
+            #     genpdf(studentName, roll, dob, age, gender, residence, lecs)
+            #
+            #     from mailto import sendmail
+            #
+            #     out1 = sendmail(emailid, studentName)
+            #     print(f"Report sent to " + "'" + emailid + "'")
+            #
+            # else:
+            #     print("Report not sent!")
 
         except Exception as val:
             print(val)
             print("This type of value is not found in the data")
+
+
+
+
